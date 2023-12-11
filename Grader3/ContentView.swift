221cb1bccd85d79092eee78fd8d1d499b828330a
Imelda Yoon <136655874+ParkBench78@users.noreply.github.com>
2023-12-10 20:53:55 -0500

    Initial Commit

---
+++ Grader3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grader3
+//
+//  Created by Imelda Yoon on 12/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grader3/Grader3App.swift
@@ -0,0 +1,17 @@
+//
+//  Grader3App.swift
+//  Grader3
+//
+//  Created by Imelda Yoon on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Grader3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
